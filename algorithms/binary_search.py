531052ca17726cd86f881fc5770cf4ce0138a6cf
Luís Henrique <lhamagalhaes@gmail.com>
2025-05-27 17:55:26 -0300

    new folder algorithms and first algorithm (bin_search)

---
+++ algorithms/binary_search.py
@@ -0,0 +1,16 @@
+import math
+def bin_search(nums, target):
+
+    l, r= 0, len(nums) - 1
+
+    while l <= r:
+        m = (l + r) // 2
+        if nums[m] == target:
+            return m
+        elif nums[m] < target:
+            l = m + 1
+        elif nums[m] > target:
+            r = m - 1        
+    return False
+
+print(bin_search([1,2,3,4,5,6,7,8,9], 2))
